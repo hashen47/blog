e26e803684a8f9f014f5f15d786702b520fe9da8
hashen47 <dilushhashen11@gmail.com>
2023-02-12 13:26:42 +0530

    edit index.js file(main file), connet the database, set views folder and initialize the view engine, configure the session, import auth route

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+require("dotenv").config(); // configure the environment variables
 const express = require("express");
+
+
+// database connection
+db = require("./database/db");
+db.connect(err => {
+    if (err)
+    {
+        console.log(err); // for debugging
+        return;
+    }
+})
 
 
 // application
@@ -6,10 +18,31 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-// simple route
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
-})
+// initialize the view engine
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+
+// configure the session
+const session = require("express-session");
+app.use(session({
+    secret : process.env.SECRET,
+    resave : false,
+    saveUninitialized : false,
+    cookie : {
+        secure : false // in the production this should change to true
+    }
+}))
+
+
+// middlewares
+app.use("/static", express.static("./public"));
+app.use(express.json());
+
+
+// routes
+const auth = require("./routes/auth");
+app.use("/", auth);
 
 
 // listening
