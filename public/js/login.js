4efcba9f4a889f8534dc358f4f90b2b5c5ca4e08
hashen47 <dilushhashen11@gmail.com>
2023-02-12 13:24:27 +0530

    edit public/js/login.js file, change the fetch url to '/'

--- public/js/login.js
+++ public/js/login.js
@@ -3,20 +3,23 @@ window.addEventListener("load", () => {
     const loginBtn = document.getElementById("login-btn");
     const uInput = document.getElementById("username");
     const pInput = document.getElementById("password");
+    const rInput = document.getElementById("remember");
+
 
     loginBtn.addEventListener("click", e => {
         console.log("happend");
         (async () => {
             try 
             {
-                const response = await fetch("/login", {
+                const response = await fetch("/", {
                     method : "POST",
                     headers : {
                         "Content-Type" : "application/json"
                     },
                     body : JSON.stringify({
                         username : uInput.value,
-                        password : pInput.value
+                        password : pInput.value,
+                        remember : rInput.checked
                     })
                 });
 
@@ -27,7 +30,7 @@ window.addEventListener("load", () => {
                 if (resp.status == "success")
                 {
                     console.log("here");
-                    window.open("/", "_self"); // if user is successfully login user redirect to the index page
+                    window.open("/posts", "_self"); // if user is successfully login user redirect to the index page
                 }
             }
             catch(err)
