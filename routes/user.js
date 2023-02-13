6d9ad4b43c57a159c38ccadf114d527c304b51ab
hashen47 <dilushhashen11@gmail.com>
2023-02-14 02:15:58 +0530

    edit routes/user.js file, newly added delete and comment routes

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,33 @@
 const { Router } = require("express");
 const { isAuthenticated } = require("../controllers/auth-ctr");
-const { create, del, update, loadPostUpdate } = require("../controllers/user-ctr");
+const { create, del, update, loadPostUpdate, myPosts, anyOnePosts, comment } = require("../controllers/user-ctr");
 const router = Router();
 
 
-router.use("/", isAuthenticated); // user should logged to go to any route of /user
 
-router.get("/create", (req, res) => {
+router.get("/create", isAuthenticated, (req, res) => {
     res.render("create", { title : "create", log : req.session.uid });
 })
 
-router.route("/create").post(create);
+router.route("/create").post(isAuthenticated, create);
 
-router.get("/:user/posts", (req, res) => {
-    res.render("index", { title : "create", log : req.session.uid });
+router.get("/posts", isAuthenticated, myPosts, (req, res) => { // load current user posts
+    res.render("index", { title : "My Posts", log : req.session.uid, posts : req.posts });
 })
 
-router.get("/posts/:puid/update", loadPostUpdate, (req, res) => {
+router.get("/:user/posts", anyOnePosts, (req, res) => { // load other one posts (according to :user)
+    res.render("index", { title : `${req.params.user}'s Posts`, log : req.session.uid, posts : req.posts });
+})
+
+router.get("/posts/:puid/update", isAuthenticated, loadPostUpdate, (req, res) => {
     res.render("update", { title : "Update", log : req.session.uid, puid : req.params.puid, post : req.post });
 })
 
-router.route("/update").post(update);
+router.route("/update").post(isAuthenticated, update);
+
+router.route("/delete").post(isAuthenticated, del);
+
+router.route("/comment").post(comment);
 
 
 
