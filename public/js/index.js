865cb4e2dc2b12c0623a0f644d8613e842862c32
hashen47 <dilushhashen11@gmail.com>
2023-02-14 22:59:42 +0530

    edit views/includes/mixins.pug and public/js/index.js files, create functionillity like or dislike to a post

--- public/js/index.js
+++ public/js/index.js
@@ -59,9 +59,86 @@ window.addEventListener("load", () => {
                 });
 
                 const resp = await response.json();
+
                 console.log(resp.msg);
+                if (resp.status == "error") alert(resp.msg);
                 if (resp.status == "success") window.location.reload();
             })();
         })
     })
+
+    // like to the post
+    // to like a post you should click dislike icon
+    document.querySelectorAll(".dislike").forEach(svg => { 
+        svg.addEventListener("click", e => {
+            const pid = svg.closest("div").getAttribute("pid");
+
+            (async() => {
+                const response = await fetch("/user/like", {
+                    method : "POST",
+                    headers : {
+                        "Content-Type" : "application/json"
+                    },
+                    body : JSON.stringify({
+                        pid : pid
+                    })
+                })
+
+                const resp = await response.json();
+                if (resp.status == "success") window.location.reload();
+                else if (resp.status == "error") alert(resp.msg);
+            })();
+        })
+    })
+
+    // dislike to the post
+    // to dislike a post you should click like icon
+    function dislike() 
+    {
+        document.querySelectorAll(".like").forEach(svg => {
+            svg.addEventListener("click", e => {
+                const pid = svg.closest("div").getAttribute("pid");
+
+                (async() => {
+                    const response = await fetch("/user/dislike", {
+                        method : "POST",
+                        headers : {
+                            "Content-Type" : "application/json"
+                        },
+                        body : JSON.stringify({
+                            pid : pid
+                        })
+                    })
+
+                    const resp = await response.json();
+                    if (resp.status == "success") window.location.reload();
+                    else if (resp.status == "error") alert(resp.msg);
+                })();
+            })
+        })
+    }
+
+
+    // check if current user like or dislike to a post
+    async function isLiked()
+    {
+        const response = await fetch("/user/isliked");
+        let resp = await response.json();
+
+        document.querySelectorAll(".like").forEach(like => {
+            if (resp.pids.includes(like.closest("div").getAttribute("pid"))) 
+            {
+                like.style.display = "inherit";
+                like.nextElementSibling.style.display = "none";
+            }
+            else 
+            {
+                like.nextElementSibling.style.display = "inherit";
+            }
+        })
+
+        dislike();
+    }
+
+    isLiked();
 })
